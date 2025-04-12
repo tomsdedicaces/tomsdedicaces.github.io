21c3f455318f09aea96e3ca2c87a263debbc11a2
CasperThePancake <60041702+CasperThePancake@users.noreply.github.com>
2025-04-12 17:29:30 +0200

    Update

--- totalDedi.js
+++ totalDedi.js
@@ -49,7 +49,7 @@ function countImageFiles() {
                 const imageFiles = data.filter(item => item.type === "file" && item.name.match(/\.(jpg|jpeg|png|gif)$/i));
                 totalCount += imageFiles.length;
                 console.log(totalCount)
-                document.getElementById("subTitle").innerHTML = "("+totalCount+" van 444 auteurs)"
+                document.getElementById("subTitle").innerHTML = "("+totalCount+" van 445 auteurs)"
             })
             .catch(error => console.error(error));
         }
